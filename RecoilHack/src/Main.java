346b5013501988fc0c0edf6eef267a3965fa942f
suzus <suzus@DESKTOP-QI7PR5V>
2020-01-14 00:25:57 +0900

    完成いいいいいいいい

--- RecoilHack/src/Main.java
+++ RecoilHack/src/Main.java
@@ -7,114 +7,115 @@ import org.jnativehook.mouse.NativeMouseInputListener;
 
 import com.fazecast.jSerialComm.SerialPort;
 
-public class Main extends Thread implements NativeMouseInputListener{
+public class Main extends Thread implements NativeMouseInputListener {
 
 	static boolean RMB;
 	static boolean LMB;
 	static boolean EMB;
-	static byte[] B0 = {100};
-	static byte[] B1 = {111};
+	static boolean flag = false;
+	static byte[] B0 = { 100 };
+	static byte[] B1 = { 111 };
 
-public void nativeMouseClicked(NativeMouseEvent e) {
+	public void nativeMouseClicked(NativeMouseEvent e) {
 
-}
-
-public void nativeMousePressed(NativeMouseEvent e) {
-	if(e.getButton()==1) {
-		LMB=true;
-	}else {
-		RMB=true;
 	}
-}
 
-public void nativeMouseReleased(NativeMouseEvent e) {
-	if(e.getButton()==1) {
-		LMB=false;
-	}else {
-		RMB=false;
+	public void nativeMousePressed(NativeMouseEvent e) {
+		if (e.getButton() == 1) {
+			LMB = true;
+		} else if (e.getButton() == 2) {
+			RMB = true;
+		}
 	}
-}
 
-public void nativeMouseMoved(NativeMouseEvent e) {
+	public void nativeMouseReleased(NativeMouseEvent e) {
+		if (e.getButton() == 1) {
+			LMB = false;
+		} else if (e.getButton() == 2) {
+			RMB = false;
+		}
+	}
 
-}
+	public void nativeMouseMoved(NativeMouseEvent e) {
 
-public void nativeMouseDragged(NativeMouseEvent e) {
+	}
 
-}
+	public void nativeMouseDragged(NativeMouseEvent e) {
 
+	}
 
 	SerialPort serialPort;
 	boolean portFound = false;
 
 	void sendByte(byte b) {
-		byte[] buff = new byte[1];
+		byte[] buff = new byte[0];
 		buff[0] = b;
 		serialPort.writeBytes(buff, 1);
 	}
 
-	  void startSerial() {
+	void startSerial() {
 
-		    // ポート名
-		    String portName = "COM6";
+		// ポート名
+		String portName = "COM6";
 
-		    for (SerialPort sp : SerialPort.getCommPorts()) {
-		      if (sp.getSystemPortName().equals(portName)) {
-		        serialPort = sp;
-		        break;
-		      }
-		    }
-		    // ボーレート
-		    //setComPortParameters​(9600, int newDataBits, int newStopBits, int newParity);
-		    serialPort.openPort();
-	  }
-
-
-
-
-
+		for (SerialPort sp : SerialPort.getCommPorts()) {
+			if (sp.getSystemPortName().equals(portName)) {
+				serialPort = sp;
+				break;
+			}
+		}
+		// ボーレート
+		//setComPortParameters​(9600, int newDataBits, int newStopBits, int newParity);
+		serialPort.openPort();
+	}
 
 	public static void main(String[] args) {
-		SerialPort comPort = SerialPort.getCommPorts()[1];
+		SerialPort comPort = SerialPort.getCommPorts()[2];
 		comPort.openPort();
 		System.out.println(comPort.getSystemPortName());
 		comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
 
-		      comPort.writeBytes(B0,1);
-		      System.out.println(B0[0]);
-		      comPort.writeBytes(B1,1);
-		      System.out.println(B1[0]);
+		comPort.writeBytes(B0, 1);
+		System.out.println(B0[0]);
+		comPort.writeBytes(B1, 1);
+		System.out.println(B1[0]);
 
 		try {
 			GlobalScreen.registerNativeHook();
-		}catch (NativeHookException ex) {
+		} catch (NativeHookException ex) {
 			System.err.println("There was a problem registering the native hook.");
 			System.err.println(ex.getMessage());
 			System.exit(1);
 		}
 
-			// Construct the example object.
-			//System.out.println("aaaa");
-			Main example = new Main() {
-				  @Override
-				  public void run() {
-					  while(true) {
-						  if(RMB&&LMB) {
-							  comPort.writeBytes(B0,1);
-						  }else {
-							  comPort.writeBytes(B1,1);
-						  }
-					  }
-				  }
-			};
-			LogManager.getLogManager().reset();
-			example.start();
+		// Construct the example object.
+		//System.out.println("aaaa");
+		Main example = new Main() {
+			@Override
+			public void run() {
+				System.out.println(flag);
+				while (true) {
+					System.out.print("");
+					if (RMB && LMB&&!flag) {
+						comPort.writeBytes(B0, 1);
+						flag = true;
+						System.out.println(flag);
+					}else if (!LMB &&flag) {
+						comPort.writeBytes(B1, 1);
+						flag = false;
+						System.out.println(flag);
+					}
+				}
+			}
+		};
+		LogManager.getLogManager().reset();
+		example.start();
 
-			// Add the appropriate listeners.
-			GlobalScreen.addNativeMouseListener(example);
+		// Add the appropriate listeners.
+		GlobalScreen.addNativeMouseListener(example);
 
-			//GlobalScreen.addNativeMouseMotionListener(example);
-			//System.out.println("cccc");
+		//GlobalScreen.addNativeMouseMotionListener(example);
+		//System.out.println("cccc");
 
 	}
 }
